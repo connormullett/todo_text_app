fc80ad5dbd57342696bec186cfe69cf17336292d
connor <connormullett@gmail.com>
2019-01-22 20:11:12 -0500

    prettied it up with clint and added rollback for error handling with db transactions

--- todo.py
+++ todo.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
-import psycopg2
 import time
+import psycopg2
+import os
+import sys
 
+from clint.textui import colored, puts
 from psycopg2 import sql
 from datetime import datetime
 
@@ -15,17 +18,19 @@ with open('settings.txt', 'r') as f:
 
 
 class Todo(object):
-    '''id: integer, title: string, content: string, due: datetime, complete: bool'''
+    '''id: integer, title: string, content: string,
+    due: datetime, complete: bool'''
     def __init__(self, year, month, day, hour, minute, *args, **kwargs):
         self.__dict__.update(kwargs)
         due = datetime(year, month, day, hour, minute)
         self.due = time.mktime(due.timetuple())
 
+
 def connect():
     global conn
     try:
-        conn = psycopg2.connect('dbname=todos user=postgres password={}'.format(PASSWORD))
-        cur = conn.cursor()
+        conn = psycopg2.connect('dbname=todos user=postgres ' +
+                                'password={}'.format(PASSWORD))
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
     finally:
@@ -34,19 +39,20 @@ def connect():
 
 
 def create_todo(year, month, day, hour, minute, **kwargs):
-    todo = Todo(year, month, day, hour, minute, **kwargs)
-    cur = conn.cursor()
-    cur.execute('''INSERT INTO todo (title, content, due, complete)
-                   values ('{}', '{}', {}, {})'''.format(todo.title, todo.content,
-                       todo.due, todo.complete))
+    try:
+        todo = Todo(year, month, day, hour, minute, **kwargs)
+        cur = conn.cursor()
+        cur.execute('''INSERT INTO todo (title, content, due, complete)
+                    values ('{}', '{}', {}, {})'''.format(todo.title,
+                    todo.content, todo.due, todo.complete))
 
-    conn.commit()
-    cur.close()
-    print('todo added')
-
-
-def update_todo(todo_id):
-    pass
+        conn.commit()
+        cur.close()
+        print('todo added')
+    except (Exception, psycopg2.DatabaseError) as error:
+        print('Error: ', error)
+        print('Rolling back transaction')
+        conn.rollback()
 
 
 def get_todos():
@@ -73,6 +79,9 @@ def get_complete_todos():
         curr.execute(sql)
         row = curr.fetchone()
 
+        if row is None:
+            return puts(colored.blue('NO TODOS COMPLETE'))
+
         while row is not None:
             todo_id, title, content, is_complete, due = row
             due_date = time.ctime(due)
@@ -80,8 +89,6 @@ def get_complete_todos():
             # TODO: Make this format pretty
             print(todo_id, title, content, complete, due_date)
             row = curr.fetchone()
-        if row is None:
-            print('No todos marked complete')
     except Exception as e:
         print('Error', e)
     curr.close()
@@ -102,49 +109,60 @@ def get_not_complete_todos():
             row = curr.fetchone()
     except Exception as e:
         print('Error', e)
+        print('Rolling back transaction')
+        conn.rollback()
     curr.close()
 
 
 def delete_todo(todo_id):
     curr = conn.cursor()
     try:
-        curr.execute(sql.SQL('''delete from todo where {} = id'''.format(todo_id)))
+        curr.execute(sql.SQL('''delete from todo where {} = id'''
+                             .format(todo_id)))
         conn.commit()
         print('todo deleted')
     except Exception:
         print('No todo found for ID')
+    except psycopg2.DatabaseError as error:
+        print('Error: ', error)
+        print('Rolling back transaction')
+        conn.rollback()
     curr.close()
 
 
 def mark_complete(todo_id):
     curr = conn.cursor()
     try:
-        curr.execute(sql.SQL('update todo set complete = true where id = {}'.format(todo_id)))
+        curr.execute(sql.SQL('update todo set complete = true' +
+                             'where id = {}'.format(todo_id)))
         conn.commit()
         print('marked todo complete')
-    except Exception as e:
+    except Exception:
         print('No todo found for ID')
     curr.close()
+
 
 def main():
     global conn
     if conn is None:
         conn = connect()
 
-    view_option = False
     while True:
-        if not view_option:
-            print('------------------------------')
-            get_todos()
-        view_option = False
-        print('------------------------------')
-        action = input('1. Create\n2. Delete\n3. Options\n')
+        os.system('clear')
+        puts(colored.cyan('==================='))
+        get_todos()
+        puts(colored.cyan('==================='))
+        action = input('1. Create\n2. Delete\n3. Options\n4. Exit\n')
         try:
             action = int(action)
         except Exception:
-            print('Invalid Argument')
+            puts(colored.red('INVALID ARGUMENT'))
             continue
         if action == 1:
+            puts(colored.cyan('--- CREATE ---'))
+            os.system('clear')
+            # TODO: Indiv check for bad answers
+            # Dont want to waste time if year is bad input
             year = int(input('year (YYYY) : '))
             month = int(input('month (MM) : '))
             day = int(input('day (dd) : '))
@@ -152,18 +170,24 @@ def main():
             minute = int(input('minute (mm) : '))
             title = input('title: ')
             content = input('content: ')
-            todo = create_todo(year, month, day, hour, minute, title=title,
-                    content=content, complete=False)
+            create_todo(year, month, day, hour, minute, title=title,
+                        content=content, complete=False)
         elif action == 2:
+            os.system('clear')
+            puts(colored.cyan('--- DELETE ---'))
             todo_id = int(input('id: '))
             delete_todo(todo_id)
         elif action == 3:
-            view_action = input('1. Get Complete\n2. Get Incomplete\n3. Mark Complete\n4. Back\n')
-            view_option = True
+            os.system('clear')
+            puts(colored.cyan('--- OPTIONS ---'))
+            view_action = input('1. Get Complete\n2. Get Incomplete' +
+                                '\n3. Mark Complete\n4. Back\n')
             try:
                 view_action = int(view_action)
             except Exception:
-                print('Invalid Argument')
+                puts(colored.red('INVALID ARGUMENT'))
+                puts(colored.red(input('press any key to continue ... ')))
+            os.system('clear')
             if view_action == 1:
                 get_complete_todos()
             elif view_action == 2:
@@ -172,14 +196,18 @@ def main():
                 todo_id = input('Enter todo id: ')
                 mark_complete(todo_id)
             elif view_action == 4:
-                pass
+                continue
             else:
                 print('Invalid Argument')
-
+            input('\n press any key to continue ... ')
+        elif action == 4:
+            conn.close()
+            sys.exit(0)
         else:
-            print('Invalid Argument')
+            puts(colored.red('INVALID ARGUMENT'))
 
 
 if __name__ == '__main__':
     main()
 
+conn.rollback()
